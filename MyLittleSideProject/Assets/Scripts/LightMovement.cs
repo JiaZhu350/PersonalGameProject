25a773563b5998f4ebe36c5bc1d958236a5fa61d
Jianan Zhu <jianaz11@uci.edu>
2025-01-04 00:05:29 -0800

    Changed the movement of the light source

--- MyLittleSideProject/Assets/Scripts/LightMovement.cs
+++ MyLittleSideProject/Assets/Scripts/LightMovement.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class LightMovement : MonoBehaviour
 {
+    [SerializeField] private float lightSpeed = 3f; 
     // Update is called once per frame
     void Update()
     {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = mousePosition;
+        transform.position = Vector2.MoveTowards(transform.position, mousePosition, lightSpeed*Time.deltaTime);
     }
 }
